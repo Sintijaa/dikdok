373169e3f0954a08175e5a6be2e6392abf805e8c
ipb21.j.puspurs <ipb21.j.puspurs@pjvt.local>
2023-10-05 09:37:56 +0300

    DIK-3 pabeigts

--- dikdok.php
+++ dikdok.php
@@ -1,1 +1,14 @@
+<?php
+Class komentārs{
+  private $id;
+  private $video_id;
+  private $autora_id;
 
+  public $teksts;
+
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+}
+?>
